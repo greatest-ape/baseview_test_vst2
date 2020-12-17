d1693bf2bc2aa8241637c28f325049f3c603fc12
Joakim Frostegård <joakim.frostegard@gmail.com>
2020-12-17 13:11:25 +0100

    Use constant for plugin name

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,8 @@ use vst::plugin::{Info, Plugin};
 use vst::editor::Editor;
 
 
+const PLUGIN_NAME: &str = "BaseviewTest";
+
 const WINDOW_WIDTH: usize = 500;
 const WINDOW_HEIGHT: usize = 500;
 
@@ -54,7 +56,7 @@ impl Editor for TestPluginEditor {
         let parent = raw_window_handle_from_parent(parent);
 
         let options = WindowOpenOptions {
-            title: "BaseviewTest".to_string(),
+            title: PLUGIN_NAME.to_string(),
             size: Size::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64),
             scale: WindowScalePolicy::ScaleFactor(1.0),
             parent: Parent::WithParent(parent),
@@ -94,7 +96,7 @@ impl Default for TestPlugin {
 impl Plugin for TestPlugin {
     fn get_info(&self) -> Info {
         Info {
-            name: "BaseviewTest".to_string(),
+            name: PLUGIN_NAME.to_string(),
             unique_id: 53435,
 
             ..Default::default()
@@ -107,7 +109,7 @@ impl Plugin for TestPlugin {
         let _ = ::std::fs::create_dir(log_folder.clone());
 
         let log_file = ::std::fs::File::create(
-            log_folder.join("BaseviewTest.log")
+            log_folder.join(format!("{}.log", PLUGIN_NAME))
         ).unwrap();
 
         let log_config = ::simplelog::ConfigBuilder::new()
