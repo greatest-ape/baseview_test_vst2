2a22e9f47f7f6eb6d4dea4ffd6bb2fb18461e5d2
DGriffin91 <33357138+DGriffin91@users.noreply.github.com>
2020-12-22 18:05:17 -0800

    Update window handler

--- src/lib.rs
+++ src/lib.rs
@@ -4,40 +4,36 @@
 extern crate vst;
 
 use baseview::{
-    Size, Event, Parent, Window, WindowHandler, WindowOpenOptions,
-    WindowScalePolicy
+    AppRunner, Event, Parent, Size, Window, WindowHandle, WindowHandler, WindowOpenOptions,
+    WindowScalePolicy,
 };
 use raw_window_handle::RawWindowHandle;
-use vst::plugin::{Info, Plugin};
 use vst::editor::Editor;
-
-
-const PLUGIN_NAME: &str = "BaseviewTest";
+use vst::plugin::{Info, Plugin};
 
 const WINDOW_WIDTH: usize = 500;
 const WINDOW_HEIGHT: usize = 500;
 
-
 #[derive(Default)]
 struct TestWindowHandler;
 
-
 impl WindowHandler for TestWindowHandler {
+    type Message = ();
+
+    fn on_message(&mut self, _: &mut Window, message: Self::Message) {
+        ::log::info!("TestWindowHandler received message: {:?}", message)
+    }
+
     fn on_event(&mut self, _: &mut Window, event: Event) {
         ::log::info!("TestWindowHandler received event: {:?}", event)
     }
 
-    fn on_frame(&mut self) {
-        
-    }
+    fn on_frame(&mut self) {}
 }
 
-
-#[derive(Default)]
 struct TestPluginEditor {
-    is_open: bool,
+    handle: Option<(WindowHandle<TestWindowHandler>, Option<AppRunner>)>,
 }
-
 
 impl Editor for TestPluginEditor {
     fn position(&self) -> (i32, i32) {
@@ -49,54 +45,49 @@ impl Editor for TestPluginEditor {
     }
 
     fn open(&mut self, parent: *mut ::std::ffi::c_void) -> bool {
-        if self.is_open {
+        if self.is_open() {
             return false;
         }
 
         let parent = raw_window_handle_from_parent(parent);
 
         let options = WindowOpenOptions {
-            title: PLUGIN_NAME.to_string(),
+            title: "BaseviewTest".to_string(),
             size: Size::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64),
-            scale: WindowScalePolicy::SystemScaleFactor,
+            scale: WindowScalePolicy::ScaleFactor(1.0),
             parent: Parent::WithParent(parent),
         };
 
-        Window::open(options, |_|{
-            TestWindowHandler::default()
-        });
+        self.handle = Some(Window::open(options, |_| TestWindowHandler::default()));
 
         true
     }
 
     fn is_open(&mut self) -> bool {
-        self.is_open
+        self.handle.is_some()
     }
 
     fn close(&mut self) {
-        self.is_open = false;
+        self.handle = None;
     }
 }
-
 
 struct TestPlugin {
     editor: Option<TestPluginEditor>,
 }
 
-
 impl Default for TestPlugin {
     fn default() -> Self {
         Self {
-            editor: Some(TestPluginEditor::default()),
+            editor: Some(TestPluginEditor { handle: None }),
         }
     }
 }
 
-
 impl Plugin for TestPlugin {
     fn get_info(&self) -> Info {
         Info {
-            name: PLUGIN_NAME.to_string(),
+            name: "BaseviewTest".to_string(),
             unique_id: 53435,
 
             ..Default::default()
@@ -104,11 +95,25 @@ impl Plugin for TestPlugin {
     }
 
     fn init(&mut self) {
-        init_logging();
-    }   
+        let log_folder = ::dirs::home_dir().unwrap().join("tmp");
+
+        let _ = ::std::fs::create_dir(log_folder.clone());
+
+        let log_file = ::std::fs::File::create(log_folder.join("BaseviewTest.log")).unwrap();
+
+        let log_config = ::simplelog::ConfigBuilder::new()
+            .set_time_to_local(true)
+            .build();
+
+        let _ = ::simplelog::WriteLogger::init(simplelog::LevelFilter::Info, log_config, log_file);
+
+        ::log_panics::init();
+
+        ::log::info!("init");
+    }
 
     fn get_editor(&mut self) -> Option<Box<dyn Editor>> {
-        if let Some(editor) = self.editor.take(){
+        if let Some(editor) = self.editor.take() {
             Some(Box::new(editor) as Box<dyn Editor>)
         } else {
             None
@@ -116,49 +121,18 @@ impl Plugin for TestPlugin {
     }
 }
 
-
-fn init_logging(){
-    let log_folder = ::dirs::home_dir().unwrap().join("tmp");
-
-    let _ = ::std::fs::create_dir(log_folder.clone());
-
-    let log_file = ::std::fs::File::create(
-        log_folder.join(format!("{}.log", PLUGIN_NAME))
-    ).unwrap();
-
-    let log_config = ::simplelog::ConfigBuilder::new()
-        .set_time_to_local(true)
-        .build();
-
-    let _ = ::simplelog::WriteLogger::init(
-        simplelog::LevelFilter::Info,
-        log_config,
-        log_file
-    );  
-
-    ::log_panics::init();
-
-    ::log::info!("init");
-}
-
-
 #[cfg(target_os = "macos")]
-fn raw_window_handle_from_parent(
-    parent: *mut ::std::ffi::c_void
-) -> RawWindowHandle {
+fn raw_window_handle_from_parent(parent: *mut ::std::ffi::c_void) -> RawWindowHandle {
     use raw_window_handle::macos::MacOSHandle;
 
     RawWindowHandle::MacOS(MacOSHandle {
-        ns_view: parent,
+        ns_view: parent as *mut ::std::ffi::c_void,
         ..MacOSHandle::empty()
     })
 }
 
-
 #[cfg(target_os = "windows")]
-fn raw_window_handle_from_parent(
-    parent: *mut ::std::ffi::c_void
-) -> RawWindowHandle {
+fn raw_window_handle_from_parent(parent: *mut ::std::ffi::c_void) -> RawWindowHandle {
     use raw_window_handle::windows::WindowsHandle;
 
     RawWindowHandle::Windows(WindowsHandle {
@@ -167,11 +141,8 @@ fn raw_window_handle_from_parent(
     })
 }
 
-
 #[cfg(target_os = "linux")]
-fn raw_window_handle_from_parent(
-    parent: *mut ::std::ffi::c_void
-) -> RawWindowHandle {
+fn raw_window_handle_from_parent(parent: *mut ::std::ffi::c_void) -> RawWindowHandle {
     use raw_window_handle::unix::XcbHandle;
 
     RawWindowHandle::Xcb(XcbHandle {
@@ -179,6 +150,5 @@ fn raw_window_handle_from_parent(
         ..XcbHandle::empty()
     })
 }
-
 
 plugin_main!(TestPlugin);
